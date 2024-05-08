2a0c98dfc76a345f69f1261cedf2afa7ea843511
Fakur Yosi Alisandro <fakuryosi.2021@student.uny.ac.id>
2024-05-08 20:13:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,19 +11,55 @@ def home():
     uploads.reverse()
     return render_template("home.html", uploads=uploads)
 
-@app.route("/upload", methods=["GET", "POST"])
-def upload_file():
+@app.route("/upload_image", methods=["GET", "POST"])
+def upload_image():
     if request.method == 'POST':
         f = request.files['file']
-        filename = secure_filename(f.filename)
-        filepath = os.path.join(app.config['UPLOAD_PATH'], filename)
+        if f.filename != '':
+            if f.filename.endswith(('.png', '.jpg', '.jpeg', '.gif')):
+                filename = secure_filename(f.filename)
+                filepath = os.path.join(app.config['UPLOAD_PATH'], filename)
 
-        # Check if the file already exists and delete it
-        existing_file = [fn for fn in os.listdir(app.config['UPLOAD_PATH']) if fn != filename]
-        if existing_file:
-            os.remove(os.path.join(app.config['UPLOAD_PATH'], existing_file[0]))
+                # Check if the file already exists and delete it
+                existing_file = [fn for fn in os.listdir(app.config['UPLOAD_PATH']) if fn != filename]
+                if existing_file:
+                    os.remove(os.path.join(app.config['UPLOAD_PATH'], existing_file[0]))
 
-        f.save(filepath)
+                f.save(filepath)
+        return redirect(url_for("home"))
+
+@app.route("/upload_video", methods=["GET", "POST"])
+def upload_video():
+    if request.method == 'POST':
+        f = request.files['file']
+        if f.filename != '':
+            if f.filename.endswith('.mp4'):
+                filename = secure_filename(f.filename)
+                filepath = os.path.join(app.config['UPLOAD_PATH'], filename)
+
+                # Check if the file already exists and delete it
+                existing_file = [fn for fn in os.listdir(app.config['UPLOAD_PATH']) if fn != filename]
+                if existing_file:
+                    os.remove(os.path.join(app.config['UPLOAD_PATH'], existing_file[0]))
+
+                f.save(filepath)
+        return redirect(url_for("home"))
+
+@app.route("/upload_model", methods=["GET", "POST"])
+def upload_model():
+    if request.method == 'POST':
+        f = request.files['file']
+        if f.filename != '':
+            if f.filename.endswith('.pt'):  # Anda dapat mengubah ekstensi file sesuai dengan kebutuhan Anda
+                filename = secure_filename(f.filename)
+                filepath = os.path.join(app.config['UPLOAD_PATH'], filename)
+
+                # Check if the file already exists and delete it
+               # existing_file = [fn for fn in os.listdir(app.config['UPLOAD_PATH']) if fn != filename]
+                #if existing_file:
+                    #os.remove(os.path.join(app.config['UPLOAD_PATH'], existing_file[0]))
+
+                f.save(filepath)
         return redirect(url_for("home"))
 
 @app.route('/static/<path:path>')
