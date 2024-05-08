e2560eec481c7afffb4fedd7dc24c1f91fc3134d
Fakur Yosi Alisandro <fakuryosi.2021@student.uny.ac.id>
2024-05-08 16:50:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,35 @@
-from flask import Flask,redirect,url_for,render_template,request
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from werkzeug.utils import secure_filename
 import os
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
-def uploader():
-        path = 'static/uploads/'
-        uploads = sorted(os.listdir(path), key=lambda x: os.path.getctime(path+x))
-        print(uploads)
-        #uploads = os.listdir('static/uploads')
-        uploads = ['uploads/' + file for file in uploads]
-        uploads.reverse()
-        return render_template("index.html",uploads=uploads)
+def home():
+    uploads = sorted(os.listdir(app.config['UPLOAD_PATH']), key=lambda x: os.path.getctime(os.path.join(app.config['UPLOAD_PATH'], x)))
+    uploads = ['uploads/' + file for file in uploads]
+    uploads.reverse()
+    return render_template("home.html", uploads=uploads)
 
-app.config['UPLOAD_PATH'] = 'static/uploads'
-
-@app.route("/upload",methods=['GET','POST'])
+@app.route("/upload", methods=["GET", "POST"])
 def upload_file():
-        if request.method == 'POST':
-                f = request.files['file']
-                print(f.filename)
-                filename = secure_filename(f.filename)
-                f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-                return redirect("/")
-if __name__=="__main__":
+    if request.method == 'POST':
+        f = request.files['file']
+        filename = secure_filename(f.filename)
+        filepath = os.path.join(app.config['UPLOAD_PATH'], filename)
+
+        # Check if the file already exists and delete it
+        existing_file = [fn for fn in os.listdir(app.config['UPLOAD_PATH']) if fn != filename]
+        if existing_file:
+            os.remove(os.path.join(app.config['UPLOAD_PATH'], existing_file[0]))
+
+        f.save(filepath)
+        return redirect(url_for("home"))
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_file(os.path.join(app.config['UPLOAD_PATH'], path))
+
+if __name__ == "__main__":
+    app.config['UPLOAD_PATH'] = 'static/uploads'
     app.run(debug=True)
